10f34a07f30fd31c2513643e535c40ff3f0311f0
stto1210@gmail.com <stto1210@gmail.com>
2023-04-22 12:28:39 +0200

    wtf

--- src/main/kotlin/no/steintokvam/powerprice/api/PriceApi.kt
+++ src/main/kotlin/no/steintokvam/powerprice/api/PriceApi.kt
@@ -6,7 +6,6 @@ import no.steintokvam.powerprice.infra.store.Store
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
